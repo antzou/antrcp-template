6cd5446327325072f71651c5ccd7be437eb1d78e
antzou <7521753+antzou@user.noreply.gitee.com>
2025-10-28 21:09:03 +0800

    菜单清理

--- bundles/antzou.template.application/src/com/antzou/application/handlers/OpenAboutHandler.java
+++ bundles/antzou.template.application/src/com/antzou/application/handlers/OpenAboutHandler.java
@@ -6,11 +6,6 @@ import org.eclipse.swt.widgets.Shell;
 public class OpenAboutHandler {
     @Execute
     public void execute(Shell shell) {
-        // 在e4中，通常通过部件服务显示部件
-        // 假设"AboutDialog"是您的部件ID
-//        partService.showPart("com.example.e4.part.about", PartState.ACTIVATE);
-    	
     	new AboutDialog().createComposite(shell);
-    	
     }
 }
